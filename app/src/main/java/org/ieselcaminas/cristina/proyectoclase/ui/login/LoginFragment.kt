d196117a20b34ef805f43f80a878718ed188f256
Criux21 <cris.gil.morales@gmail.com>
2020-02-21 20:40:02 +0100

    aitor es tonto

---
+++ app/src/main/java/org/ieselcaminas/cristina/proyectoclase/ui/login/LoginFragment.kt
@@ -0,0 +1,36 @@
+package org.ieselcaminas.cristina.proyectoclase.ui.login
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+
+import org.ieselcaminas.cristina.proyectoclase.R
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginFragment()
+    }
+
+    private val viewModel by lazy{
+        ViewModelProvider(this).get(LoginViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+}
