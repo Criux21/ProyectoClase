bf1fbaf02f30a2243280b870d48fce0a496a2b77
Criux21 <cris.gil.morales@gmail.com>
2020-02-22 13:36:56 +0100

    aitor es tonto

--- app/src/main/java/org/ieselcaminas/cristina/proyectoclase/ui/login/LoginFragment.kt
+++ app/src/main/java/org/ieselcaminas/cristina/proyectoclase/ui/login/LoginFragment.kt
@@ -1,5 +1,7 @@
 package org.ieselcaminas.cristina.proyectoclase.ui.login
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +10,18 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login_fragment.*
 import org.ieselcaminas.cristina.proyectoclase.R
+import org.ieselcaminas.cristina.proyectoclase.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
 
     companion object {
         fun newInstance() = LoginFragment()
+        private const val RC_SIGN_IN = 123
     }
     private lateinit var binding: LoginFragment
     private val viewModel by lazy{
@@ -24,12 +32,14 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<LoginFragment>(
+        val binding = DataBindingUtil.inflate<LoginFragmentBinding>(
             inflater, R.layout.login_fragment, container, false
         )
 
-       // binding.authButton.setOnClickListener { launchSignInFlow() }
-
+       //binding.authButton.setOnClickListener { launchSignInFlow() }
+        binding.loginbutton.setOnClickListener {
+            createSingIn()
+        }
         return binding.root
 
     }
@@ -38,6 +48,43 @@ class LoginFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         // TODO: Use the ViewModel
+
+
+    }
+
+    private fun createSingIn(){
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+// Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 
 }
--- app/src/main/java/org/ieselcaminas/cristina/proyectoclase/ui/login/LoginViewModel.kt
+++ app/src/main/java/org/ieselcaminas/cristina/proyectoclase/ui/login/LoginViewModel.kt
@@ -4,4 +4,7 @@ import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+
+
 }
